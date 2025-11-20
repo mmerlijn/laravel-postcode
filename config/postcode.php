78843bf1c7e2499847b13e36bfc139f147abec17
Menno <menno@mmerlijn.nl>
2025-11-20 22:02:32 +0100

    fix for saltnet

--- config/postcode.php
+++ config/postcode.php
@@ -1,7 +1,7 @@
 <?php
 return [
-    'database_connection_name' => 'mysql_postcode',
-    'postcode_table_name' => 'postcode_nl',
+    'database_connection_name' => env('postcode_db_connection', 'mysql_postcode'),
+    'postcode_table_name' => env('postcode_table', 'postcode_nl'),
 
     //if empty not found postcodes will not be stored
     'postcode_table_not_found' => 'postcode_not_found',
