c893b1bf1b4f2febc85913c3a84684c78e4d38bd
Menno <menno@mmerlijn.nl>
2025-11-21 07:42:10 +0100

    fix tables

--- database/migrations/2020_10_10_000000_create_postcode_table.php
+++ database/migrations/2020_10_10_000000_create_postcode_table.php
@@ -16,27 +16,30 @@ return new class extends Migration {
         if (empty(config('postcode.postcode_table_name'))) {
             throw new \Exception('Error: config/postcode.php not loaded. Run [php artisan config:clear] and try again.');
         }
-        Schema::connection((config('app.env') == 'testing') ? 'sqlite' : config('postcode.database_connection_name'))->dropIfExists(config('postcode.postcode_table_name'));
-        Schema::connection((config('app.env') == 'testing') ? 'sqlite' : config('postcode.database_connection_name'))->create(config('postcode.postcode_table_name'), function (Blueprint $table) {
-            $table->increments('id');
-            $table->string('postcode', 10);
-            $table->unsignedSmallInteger('pnum')->nullable();
-            $table->string('pchar', 10)->nullable();
-            $table->unsignedSmallInteger('minnumber');
-            $table->unsignedSmallInteger('maxnumber');
-            $table->string('numbertype', 10);
-            $table->string('street', 100);
-            $table->string('city', 100);
-            $table->string('municipality', 100)->nullable();
-            $table->unsignedSmallInteger('municipality_id')->nullable();
-            $table->string('province', 30);
-            $table->string('province_code', 2)->nullable();
-            $table->double('lat')->nullable();
-            $table->double('lon')->nullable();
-            $table->double('rd_x')->nullable();
-            $table->double('rd_y')->nullable();
-            $table->index(['postcode', 'minnumber', 'maxnumber', 'numbertype'], 'pcd_ind_pcd_min_max');
-        });
+        if (config('postcode.postcode_table_name')) {
+            if (!Schema::connection((config('app.env') == 'testing') ? 'sqlite' : config('postcode.database_connection_name'))->hasTable(config('postcode.postcode_table_name'))) {
+                Schema::connection((config('app.env') == 'testing') ? 'sqlite' : config('postcode.database_connection_name'))->create(config('postcode.postcode_table_name'), function (Blueprint $table) {
+                    $table->increments('id');
+                    $table->string('postcode', 10);
+                    $table->unsignedSmallInteger('pnum')->nullable();
+                    $table->string('pchar', 10)->nullable();
+                    $table->unsignedSmallInteger('minnumber');
+                    $table->unsignedSmallInteger('maxnumber');
+                    $table->string('numbertype', 10);
+                    $table->string('street', 100);
+                    $table->string('city', 100);
+                    $table->string('municipality', 100)->nullable();
+                    $table->unsignedSmallInteger('municipality_id')->nullable();
+                    $table->string('province', 30);
+                    $table->string('province_code', 2)->nullable();
+                    $table->double('lat')->nullable();
+                    $table->double('lon')->nullable();
+                    $table->double('rd_x')->nullable();
+                    $table->double('rd_y')->nullable();
+                    $table->index(['postcode', 'minnumber', 'maxnumber', 'numbertype'], 'pcd_ind_pcd_min_max');
+                });
+            }
+        }
         if (config('postcode.postcode_table_not_found')) {
             if (!Schema::connection((config('app.env') == 'testing') ? 'sqlite' : config('postcode.database_connection_name'))->hasTable(config('postcode.postcode_table_not_found'))) {
                 Schema::connection((config('app.env') == 'testing') ? 'sqlite' : config('postcode.database_connection_name'))->create(config('postcode.postcode_table_not_found'), function (Blueprint $table) {
